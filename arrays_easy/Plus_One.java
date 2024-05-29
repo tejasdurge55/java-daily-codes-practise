61c4ea99b0abc7afcdf43b469b2e1ee4d9490cfd
tejasdurge55 <tejas.y.durge@gmail.com>
2024-05-29 08:47:30 +0530

    deleted unneccessary files

--- arrays_easy/Plus_One.java
+++ arrays_easy/Plus_One.java
@@ -21,77 +21,78 @@
 //Incrementing by one gives 9 + 1 = 10.
 //Thus, the result should be [1,0].
 
-
 package arrays_easy;
+
 import java.util.*;
+
 public class Plus_One {
     public static void main(String[] args) {
-        int[] digits = {8,9,9,9};
-        System.out.println(Arrays.toString(plusOne( digits) ));
+        int[] digits = { 8, 9, 9, 9 };
+        System.out.println(Arrays.toString(plusOne(digits)));
     }
+
     static int[] plusOne(int[] digits) {
-        ArrayList<Integer> arr1=new ArrayList<> ();
-        digits[digits.length-1]=digits[digits.length-1]+1;
-//        if(digits[digits.length-1]==9){
-//            digits[digits.length-1]=1;
-            for(int i=digits.length-1;i>=0;i--){
-                if(digits[i]==10){
-                    if(i==0){
-                        digits[i]=0;
-                        arr1.add(1);
-                        break;
-                    }
-                    digits[i]=0;
-                    digits[i-1]=digits[i-1]+1;
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        digits[digits.length - 1] = digits[digits.length - 1] + 1;
+        // if(digits[digits.length-1]==9){
+        // digits[digits.length-1]=1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 10) {
+                if (i == 0) {
+                    digits[i] = 0;
+                    arr1.add(1);
+                    break;
                 }
-                else{break;}
+                digits[i] = 0;
+                digits[i - 1] = digits[i - 1] + 1;
+            } else {
+                break;
             }
-            for (int i = 0; i < digits.length; i++) {
-                arr1.add(digits[i]);
-            }
-            
-       
-            //arr1.add(0);
-            int[] ans =new int[arr1.size()];
-            for (int i = 0; i < arr1.size(); i++) {
-                ans[i]=arr1.get(i);
-            }
-            return ans;
         }
-//        else{
-//            for (int i = 0; i < digits.length-1; i++) {
-//                arr1.add(digits[i]);
-//            }
-//            arr1.add(digits[digits.length-1]+1);
-//            int[] ans =new int[arr1.size()];
-//            for (int i = 0; i < arr1.size(); i++) {
-//                ans[i]=arr1.get(i);
-//            }
-//            return ans;
-//        }
-//        long  sum=0;
-//        
-//        for (int i = 0; i < digits.length; i++) {
-//            sum=sum+(digits[i]*((long)Math.pow(10, digits.length-1-i)));
-//            //System.out.println(sum);
-//        }
-//        //System.out.println(sum);
-//        sum=sum+1;
-//        //System.out.println(sum);
-//        double sum2=0;
-//        while(sum>0){
-//            //int a=(int)()
-//            arr1.add((int)(sum%10));
-//            //System.out.println(sum%10);
-//            sum=sum/10;
-//            //System.out.println(sum);
-//            //System.out.println(arr1.toString());
-//        }
-//        //System.out.println(arr1.toString());
-//        int[] ans =new int[arr1.size()];
-//        for (int i = 0; i < arr1.size(); i++) {
-//            ans[i]=arr1.get(arr1.size()-1-i);
-//        }
-        //return ans;
+        for (int i = 0; i < digits.length; i++) {
+            arr1.add(digits[i]);
+        }
+
+        // arr1.add(0);
+        int[] ans = new int[arr1.size()];
+        for (int i = 0; i < arr1.size(); i++) {
+            ans[i] = arr1.get(i);
+        }
+        return ans;
     }
+    // else{
+    // for (int i = 0; i < digits.length-1; i++) {
+    // arr1.add(digits[i]);
+    // }
+    // arr1.add(digits[digits.length-1]+1);
+    // int[] ans =new int[arr1.size()];
+    // for (int i = 0; i < arr1.size(); i++) {
+    // ans[i]=arr1.get(i);
+    // }
+    // return ans;
+    // }
+    // long sum=0;
+    //
+    // for (int i = 0; i < digits.length; i++) {
+    // sum=sum+(digits[i]*((long)Math.pow(10, digits.length-1-i)));
+    // //System.out.println(sum);
+    // }
+    // //System.out.println(sum);
+    // sum=sum+1;
+    // //System.out.println(sum);
+    // double sum2=0;
+    // while(sum>0){
+    // //int a=(int)()
+    // arr1.add((int)(sum%10));
+    // //System.out.println(sum%10);
+    // sum=sum/10;
+    // //System.out.println(sum);
+    // //System.out.println(arr1.toString());
+    // }
+    // //System.out.println(arr1.toString());
+    // int[] ans =new int[arr1.size()];
+    // for (int i = 0; i < arr1.size(); i++) {
+    // ans[i]=arr1.get(arr1.size()-1-i);
+    // }
+    // return ans;
 }
--- competitive_programming_or_random_problems/README.md
+++
@@ -1,1 +0,0 @@
-# java-daily-codes
